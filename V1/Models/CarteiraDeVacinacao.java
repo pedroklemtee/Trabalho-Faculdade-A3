1d5dff0ddcc31db5f234a28b8f60338b7782b60f
Pedro Klemt <157625587+pedroklemtee@users.noreply.github.com>
2024-07-02 19:57:55 -0300

    Update CarteiraDeVacinacao

--- V1/Models/CarteiraDeVacinacao.java
+++ V1/Models/CarteiraDeVacinacao.java
@@ -1,7 +1,11 @@
 package Models;
 
 import java.util.Date;
+import java.util.List;
 
 public class CarteiraDeVacinacao {
-    
+    private String endereco;
+    private int telefone;
+    private Diretor diretor;
+    private List<Enfermeira>();
 }
